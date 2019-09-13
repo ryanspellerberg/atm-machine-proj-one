fd465b1feb40a1efb8efbbb7837c6af6eab33d14
unknown <ryanspellerberg@gmail.com>
2019-09-12 19:32:48 -0500

    taking user input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Welcome to the ATM!");
+        System.out.println("Please enter a four digit pin to create a new account");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String input = scanner.nextLine();
+            System.out.println(input);
+        } catch (IllegalStateException ex) {
+            System.out.println(ex);
+        }
     }
 }
+// Create new pin
+// create new customer
